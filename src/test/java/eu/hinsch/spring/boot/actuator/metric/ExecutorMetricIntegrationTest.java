137be0cc55aa18c2b6452bee7dfc6c105b640731
Lukas Hinsch <lukas@hinsch.eu>
2015-04-25 19:41:17 +0200

    Integrationtest

---
+++ src/test/java/eu/hinsch/spring/boot/actuator/metric/ExecutorMetricIntegrationTest.java
@@ -0,0 +1,73 @@
+package eu.hinsch.spring.boot.actuator.metric;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ * Created by lh on 25/04/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ExecutorMetricIntegrationTest.TestConfig.class)
+@WebAppConfiguration
+public class ExecutorMetricIntegrationTest {
+
+    private ExecutorMetric executorMetric;
+    private MockMvc mockMvc;
+
+    @SpringBootApplication
+    static class TestConfig {
+        @Bean
+        public ExecutionMetricFactory executionMetricFactory(CounterService gaugeService, GaugeService counterService) {
+            return new ExecutionMetricFactory(gaugeService, counterService);
+        }
+    }
+
+    @Autowired
+    private ExecutionMetricFactory executionMetricFactory;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        executorMetric = executionMetricFactory.executorMetric("test");
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldExposeMeasurementAsMetrics() throws Exception {
+        // when
+        executorMetric.measure(() -> sleep(50));
+        executorMetric.measure(() -> sleep(100));
+
+        // then
+        mockMvc.perform(get("/metrics"))
+                .andExpect(jsonPath("$.['gauge.test.last']").value(greaterThanOrEqualTo(100.0)))
+                .andExpect(jsonPath("$.['gauge.test.average']").value(both(greaterThanOrEqualTo(75.0)).and(lessThan(100.0))))
+                .andExpect(jsonPath("$.['gauge.test.min']").value(both(greaterThanOrEqualTo(50.0)).and(lessThan(75.0))))
+                .andExpect(jsonPath("$.['gauge.test.max']").value(greaterThanOrEqualTo(100.0)))
+                .andExpect(jsonPath("$.['counter.test']").value(2));
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {}
+    }
+}
